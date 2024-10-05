afe2f468bc62e07c6527ab5dde7a12c6ca8692eb
Dishant Sharma <dishu5570@gmail.com>
2024-10-05 18:36:49 +0530

    build

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,93 @@
-import Image from "next/image";
+"use client"
+import { cn } from "@/lib/utils";
+
+import PDFViewer from "@/components/PDFViewer";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import { useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [files, setFiles] = useState<{
+    file: File;
+    type: "pdf" | "image";
+  }[]>([]);
+  const [activeFile, setActiveFile] = useState<{
+    file: File;
+    type: "pdf" | "image";
+  } | null>(null);
+  const ref = useRef<HTMLInputElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+
+  return (
+   <div className="w-full min-h-[100dvh] flex justify-center items-center">
+    <input  ref={ref} type="file" multiple onChange={(e) => {
+      if (!e.target.files) return;
+      const files = Array.from(e.target.files);
+      const processedFiles = files.map((file) => {
+        return {
+          file,
+          type: (file.type.includes("pdf") ? "pdf" : "image") as "pdf" | "image",
+        };
+      })
+      setFiles(existingFiles => [...existingFiles, ...processedFiles]);
+      setActiveFile({
+        file: files?.[0],
+        type: files?.[0].type.includes("pdf") ? "pdf" : "image",
+      });
+    }} 
+    accept="application/pdf,image/*"
+    className="hidden" />
+   {files.length<1 &&  <Button onClick={()=>{
+      ref.current?.click();
+    }}>
+      Select PDF or Image Files
+    </Button>}
+    {activeFile && activeFile.type === "pdf" && (
+      <PDFViewer pdfSource={activeFile.file} />
+    )}
+    {activeFile && activeFile.type === "image" && (
+      <img 
+      className="max-w-[90vw] max-h-[90vh] object-contain"
+      src={URL.createObjectURL(activeFile.file)} alt={activeFile.file.name} />
+    )}
+    <div className={cn("w-[20vw] z-10 bg-background transition-all duration-200 h-[100vh] fixed top-0  border border-input", {
+      "right-0": isOpen,
+      "right-[-20vw]": !isOpen,
+    })}>
+      <div className="w-fullrelative h-full p-[1rem]">
+        <div onClick={()=>{
+          setIsOpen(!isOpen);
+        }} className={cn("p-[0.5rem] z-[100] transition duration-200 cursor-pointer absolute top-[1rem] left-0 transform  w-fit bg-primary border border-input rounded-md", {
+          "-translate-x-1/2": isOpen,
+          "-translate-x-full rounded-r-none": !isOpen,
+        })}>
+          <ChevronLeft size={24} className="text-background" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex overflow-y-auto max-h-[90vh] flex-col gap-4">
+          <Button onClick={()=>{
+            ref.current?.click();
+          }
+          }>
+            Select PDF or Image Files
+          </Button>
+          {
+            files.map((file, index) => {
+              return (
+                <div key={index} onClick={() => {
+                  setActiveFile(file);
+                }} className={cn("p-[1rem] cursor-pointer border border-input rounded-md", {
+                  "bg-primary text-background": activeFile?.file.name === file.file.name,
+                })}>
+                  {file.file.name}
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
     </div>
+   </div>
   );
 }
