904f2fb8916634eb5aac33253079253bfd2e7dad
Dishant Sharma <dishu5570@gmail.com>
2024-10-12 01:15:55 +0530

    images

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 
 export default function Home() {
-  const [images, setImages] = useState([
+  const [images] = useState([
     "https://picsum.photos/id/0/5000/3333",
     "https://picsum.photos/id/1/5000/3333",
     "https://picsum.photos/id/2/5000/3333",
