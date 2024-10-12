5e84d829714226f3582f4bef2bfcbfc0d20a6498
Dishant Sharma <dishu5570@gmail.com>
2024-10-12 15:37:40 +0530

    images

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,82 @@
 "use client"
 
 import { cn } from "@/lib/utils";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+/*
+SB
+DIOL
+DIDO
+MET
+GEST
+NORM
+LET
+CAL
+FOL
+NVP
+IRO
+THANKYOU
+*/
 
 export default function Home() {
   const [images] = useState([
-    "https://picsum.photos/id/0/5000/3333",
-    "https://picsum.photos/id/1/5000/3333",
-    "https://picsum.photos/id/2/5000/3333",
-    "https://picsum.photos/id/3/5000/3333",
-    "https://picsum.photos/id/4/5000/3333",
+    "SB",
+    "DIOL",
+    "DIDO",
+    "MET",
+    "GEST",
+    "NORM",
+    "LET",
+    "CAL",
+    "FOL",
+    "NVP",
+    "IRO",
+    "END"
   ]);
   const [selectedImage, setSelectedImage] = useState(0);
 
+  useEffect(() => {
+    //double tap or click to fullscreen
+    const handleDoubleClick = () => {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      } else {
+        document.documentElement.requestFullscreen();
+      }
+    };
+    window.addEventListener("dblclick", handleDoubleClick);
+    //double tap on mobile
+    window.addEventListener("touchstart", (e) => {
+      if (!e.touches.length) return;
+      if (e.touches.length === 2) {
+        handleDoubleClick();
+      }
+    });
+    return () => {
+      window.removeEventListener("dblclick", handleDoubleClick);
+      window.removeEventListener("touchstart", handleDoubleClick);
+    };
+  }
+  , []);
+
   return (
-   <div className="w-full select-none min-h-[100dvh] flex justify-center items-center">
+   <div className="w-full select-none h-[100dvh] flex justify-center items-center">
 
     <img
-      src={images[selectedImage]}
-      className="w-full h-full object-contain"
+      src={`/${images[selectedImage]}.webp`}
+      className="w-auto h-full object-contain"
     />
    
-   <div className="w-full flex fixed bottom-0 left-0">
+   <div className="h-full flex flex-col fixed top-0 right-0">
     {
-      images.map((_, index) => (
+      images.map((img, index) => (
         <div
           onClick={() => setSelectedImage(index)}
          
-        key={index} className={cn("px-4 py-2 cursor-pointer bg-primary text-secondary border border-input", {
+        key={index} className={cn("py-2 text-xs text-center cursor-pointer bg-primary text-secondary border border-input", {
           "bg-secondary text-primary": selectedImage === index,
         })}>
-          Image {index}
+          {img}
         </div>
       ))
     }
